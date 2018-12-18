7215eaec8a614799630b29733406a07403901afd
absurdtheory <38075151+absurdtheory@users.noreply.github.com>
2018-12-18 14:04:21 -0500

    Update dbcon.js

--- Database Project/dbcon.js
+++ Database Project/dbcon.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_tuckera6',
-  password        : '5156',
-  database        : 'cs340_tuckera6',
+  host            : '[host name]',
+  user            : '[username]',
+  password        : '[password]',
+  database        : '[database name]',
 
  connectTimeout: 60 * 60 * 1000,
  aquireTimeout: 60 * 60 * 1000,
